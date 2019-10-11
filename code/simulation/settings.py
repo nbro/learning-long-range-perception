9c18bbd6ee049a6d8fe0fbfbfca534b1db4d58ed
Nelson Brochado <nelson.brochado@outlook.com>
2019-10-12 01:18:06 +0200

    comment added

--- code/simulation/settings.py
+++ code/simulation/settings.py
@@ -1,1 +1,3 @@
+# The coordinates to predict the output of the long-range sensor based on the output of the short-range sensor at a
+# different time step.
 target_coordinates = [(i + 100, j) for i in range(-400, 400 + 1, 50) for j in range(-400, 400 + 1, 50)]
