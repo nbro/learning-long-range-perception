d76715a3abb8f47ee025dfc36999f7a0bdcc65a2
Nelson Brochado <nelson.brochado@outlook.com>
2019-10-12 01:17:02 +0200

    removed unused variable

--- code/simulation/settings.py
+++ code/simulation/settings.py
@@ -1,2 +1,1 @@
 target_coordinates = [(i + 100, j) for i in range(-400, 400 + 1, 50) for j in range(-400, 400 + 1, 50)]
-target_coordinates_strings = [str(x) + '_' + str(y) for x, y in target_coordinates]
